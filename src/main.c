f4c0c2eb926881a794eeb42f358ffc6407331843
Rodrigo Becerril Ferreyra <becerrilferreyra.rodrigo@gmail.com>
2026-05-26 23:31:28 -0700

    Added test blinky "hello world" program
    This program flashes PC13, which on the Black Pill, is connected to the on-board user LED.

--- src/main.c
+++ src/main.c
@@ -1,4 +1,33 @@
 #include "stm32f4xx.h"
-int main(void)
+
+void setup(void);
+void delay(void);
+
+void main(void)
 {
+    setup();
+    while(1)
+    {
+        GPIOC->ODR |= GPIO_ODR_OD13;
+        delay();
+        GPIOC->ODR &= ~GPIO_ODR_OD13;
+        delay();
+    }
+}
+
+void setup(void)
+{
+    // enable GPIOC
+    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN;
+    // set GPIOC Pin 13 as output
+    GPIOC->MODER &= ~GPIO_MODER_MODER13_Msk;
+    GPIOC->MODER |= GPIO_MODER_MODER13_0;
+}
+
+void delay(void)
+{
+    for(unsigned int i = 0; i < 1000000; ++i)
+    {
+
+    }
 }
